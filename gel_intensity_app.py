00b3f67cc1dc54b184f84439862680aa9ae72f92
comparativechrono <80172051+comparativechrono@users.noreply.github.com>
2024-05-02 12:27:56 +0100

    Update gel_intensity_app.py

--- gel_intensity_app.py
+++ gel_intensity_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import numpy as np
-from PIL import Image, ImageOps, ImageDraw
+from PIL import Image, ImageOps
+import matplotlib.pyplot as plt
 
-def calculate_intensity(roi):
-    """ Calculate the mean intensity of the selected ROI """
+def calculate_intensity(image, roi_coords):
+    """Calculate the mean intensity of the selected ROI"""
+    x_start, y_start, width, height = roi_coords
+    roi = image.crop((x_start, y_start, x_start + width, y_start + height))
     return np.mean(np.array(roi))
 
 def main():
@@ -17,31 +20,46 @@ def main():
         invert = st.checkbox("Invert image colors")
         if invert:
             image = ImageOps.invert(image)
-        
-        # Displaying the image
-        img_array = np.array(image)
-        st.image(img_array, caption='Uploaded Image.', use_column_width=True)
 
-        # ROI selection
-        roi_details = st.text_input("Enter ROI coordinates as x_start, y_start, width, height", "0,0,100,100")
-        try:
-            x_start, y_start, width, height = map(int, roi_details.split(','))
-            x_end = x_start + width
-            y_end = y_start + height
-            if x_end <= image.width and y_end <= image.height:
-                img_copy = image.copy()
-                draw = ImageDraw.Draw(img_copy)
-                draw.rectangle([x_start, y_start, x_end, y_end], outline="red", width=2)
-                st.image(img_copy, caption='ROI Selected', use_column_width=True)
-                
-                if st.button('Calculate Intensity'):
-                    roi = image.crop((x_start, y_start, x_end, y_end))
-                    intensity = calculate_intensity(roi)
-                    st.write(f'ROI Intensity: {intensity}')
-            else:
-                st.error("ROI coordinates are out of image bounds.")
-        except:
-            st.error("Invalid ROI format. Please enter as x_start, y_start, width, height.")
+        img_array = np.array(image)
+        fig, ax = plt.subplots()
+        ax.imshow(img_array, cmap='gray')
+
+        # Store multiple ROIs
+        rois = []
+
+        # Define interface for multiple ROIs
+        if 'roi_list' not in st.session_state:
+            st.session_state.roi_list = []
+
+        # Add ROI with button
+        with st.form("roi_form"):
+            col1, col2 = st.columns(2)
+            with col1:
+                x_start = st.number_input('Start X', min_value=0, max_value=image.width, value=0)
+                y_start = st.number_input('Start Y', min_value=0, max_value=image.height, value=0)
+            with col2:
+                width = st.number_input('Width', min_value=1, max_value=image.width - x_start, value=100)
+                height = st.number_input('Height', min_value=1, max_value=image.height - y_start, value=100)
+            
+            submit_button = st.form_submit_button("Add ROI")
+            if submit_button:
+                st.session_state.roi_list.append((x_start, y_start, width, height))
+
+        # Draw all ROIs on the image
+        for roi in st.session_state.roi_list:
+            rect = plt.Rectangle((roi[0], roi[1]), roi[2], roi[3], linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect)
+        st.pyplot(fig)
+
+        # Display intensities for all ROIs
+        if st.button('Calculate Intensities for All ROIs'):
+            results = []
+            for roi in st.session_state.roi_list:
+                intensity = calculate_intensity(image, roi)
+                results.append(f'ROI at ({roi[0]}, {roi[1]}, {roi[2]}, {roi[3]}) - Intensity: {intensity}')
+            for result in results:
+                st.write(result)
 
 if __name__ == '__main__':
     main()
